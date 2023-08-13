0419a822b8e17791117b0a066cfd8be918677ff3
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-08-13 15:48:06 +0100

    Readme update (#1)
    * adding initial content for training and scoring
    
    * updating readme and scripts
    
    * updating TOC
    
    * minor fixes

--- score.py
+++ score.py
@@ -1,36 +1,57 @@
-import joblib
+""" model online endpoint scoring script"""
+
+import os
+import json
 import pandas as pd
+import joblib
 
-# Load the trained model
-model = joblib.load('model.joblib')
 
-# print(train_df.head())
-#    PassengerId  Survived  Pclass   Age  SibSp  Parch     Fare  Sex_female  Sex_male  Embarked_C  Embarked_Q  Embarked_S
-# 0            1       0.0       3  22.0      1      0   7.2500       False      True       False       False        True
-# 1            2       1.0       1  38.0      1      0  71.2833        True     False        True       False       False
-# 2            3       1.0       3  26.0      0      0   7.9250        True     False       False       False        True
-# 3            4       1.0       1  35.0      1      0  53.1000        True     False       False       False        True
-# 4            5       0.0       3  35.0      0      0   8.0500       False      True       False       False        True
+def init():
+    """
+    This function is called when the container is initialized/started
+    """
+    global model
 
-# Create a new dataframe with the same features as the training data
-new_data = pd.DataFrame({
-    'PassengerId': [999],
-    'Pclass': [3],
-    'Age': [25],
-    'SibSp': [1],
-    'Parch': [0],
-    'Fare': [10],
-    'Sex_female': [1],
-    'Sex_male': [0],
-    'Embarked_C': [0],
-    'Embarked_Q': [1],
-    'Embarked_S': [0],
-})
+    model_path = os.path.join(os.environ.get("AZUREML_MODEL", "model.joblib"))
+    # deserialize the model file back into a sklearn model
+    model = joblib.load(model_path)
 
-# Make predictions for the new data
-predictions = model.predict(new_data)
 
-if predictions[0] == 1:
-    print("Passenger Survived")
-else:
-    print("Passenger did not Survive")
+def run(raw_data):
+    """
+    This function is called for every invocation of the endpoint
+    to perform the actual scoring/prediction.
+    """
+
+    json_data = json.loads(raw_data)
+    data = json_data[0]
+
+    try:
+        features = [
+            "PassengerId",
+            "Pclass",
+            "Age",
+            "SibSp",
+            "Parch",
+            "Fare",
+            "Sex_female",
+            "Sex_male",
+            "Embarked_C",
+            "Embarked_Q",
+            "Embarked_S",
+        ]
+
+        prediction_data = [data[feature] for feature in features]
+        prediction_df = pd.DataFrame([prediction_data], columns=features)
+        print("prediction_data: %s", prediction_data)
+
+        result = model.predict(prediction_df)
+        print("Result: %s", result)
+
+        # create a JSON response object
+        prediction = {"prediction": int(result[0])}
+        return prediction
+
+    except ValueError as err:
+        print(str(err))
+        return str(err)
